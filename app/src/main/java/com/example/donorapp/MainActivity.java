c6961f53c15dcdc290f321dc1739c1605f41d3c2
MatthewDBarrett <12948612@student.uts.edu.au>
2019-04-10 17:43:02 +1000

    Login UI created and completed, Login class file created with a listener for the login button

--- app/src/main/java/com/example/donorapp/MainActivity.java
+++ app/src/main/java/com/example/donorapp/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.donorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText username;
+    EditText password;
+    CardView loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        username = findViewById(R.id.usernameET);
+        password = findViewById(R.id.passwordET);
+        loginBtn = findViewById(R.id.loginCV);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = String.valueOf( username.getText() );
+                String pass = String.valueOf( password.getText() );
+                String both = "Username: " + user + " Password: " + pass;
+                Toast.makeText(MainActivity.this, both, Toast.LENGTH_SHORT).show();
+
+                //CODE TO AUTHENTICATE LOGIN DETAILS
+
+                //Intent intent = new Intent(MainActivity.this, //target class);
+                //startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
