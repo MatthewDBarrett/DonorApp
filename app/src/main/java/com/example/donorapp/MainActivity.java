1f186edfcb44f77be584b8395aec8340cb0f0ed8
MatthewDBarrett <12948612@student.uts.edu.au>
2019-04-17 21:30:36 +1000

    Dialog popup added so that a choice between donors and charities is given when registering, warnings have been fixed for the registration page and a second registration page has been created, 1 for donors, 1 for charities.

--- app/src/main/java/com/example/donorapp/MainActivity.java
+++ app/src/main/java/com/example/donorapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.donorapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,6 +23,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseUser;
+
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,61 +49,57 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 String user = String.valueOf( username.getText() );
                 String pass = String.valueOf( password.getText() );
-                String both = "Username: " + user + " Password: " + pass;
-                Toast.makeText(MainActivity.this, both, Toast.LENGTH_SHORT).show();
-
-                //CODE TO AUTHENTICATE LOGIN DETAILS
                 logIn(user, pass);
-
-//                Intent intent = new Intent(MainActivity.this, HomePage.class);
-//                startActivity(intent);
             }
         });
 
         registrationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
-                String user = String.valueOf( username.getText() );
-                String pass = String.valueOf( password.getText() );
-                String both = "Username: " + user + " Password: " + pass;
-                Toast.makeText(MainActivity.this, both, Toast.LENGTH_SHORT).show();
-
-                //CODE TO AUTHENTICATE LOGIN DETAILS
-
-                Intent intent = new Intent(MainActivity.this, RegistrationPage.class);
-                startActivity(intent);
-            }
+            public void onClick(View view) { showDialog(MainActivity.this,"User type?", null);    }
         });
-
     }
 
-    public void logIn(String username, String password)
-    {
-        if (username.isEmpty() || password.isEmpty())
-        {
+    public void logIn(String username, String password) {
+        if (username.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "The username and password fields are required.", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        final Intent intent = new Intent(this, RegistrationPage.class); // HomePage.class);
+        final Intent intent = new Intent(this, HomePage.class);
 
         mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful())
-                {
+                if (task.isSuccessful()) {
                     startActivity(intent);
-                }
-                else
-                {
+                } else {
                     Toast.makeText(MainActivity.this, "Wrong Credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    public void showDialog(Activity activity, String title, CharSequence message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity, R.style.DialogTheme);
+
+        if (title != null) builder.setTitle(title);
+
+        builder.setMessage(message);
+        builder.setPositiveButton("Donor", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(getApplicationContext(), DonorRegistrationPage.class);
+                startActivity(intent);
+            }
+        });
+        builder.setNeutralButton("Charity", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(getApplicationContext(), CharityRegistrationPage.class);
+                startActivity(intent);
+            }
+        });
+        builder.show();
     }
 
 
