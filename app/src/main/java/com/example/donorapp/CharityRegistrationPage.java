b81c65f845bf6b7a4945c16b651a76fe144a814a
MatthewDBarrett <12948612@student.uts.edu.au>
2019-04-23 22:16:48 +1000

    EditText variables assigned to the charity registration page

--- app/src/main/java/com/example/donorapp/CharityRegistrationPage.java
+++ app/src/main/java/com/example/donorapp/CharityRegistrationPage.java
@@ -1,13 +1,59 @@
 package com.example.donorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CharityRegistrationPage extends AppCompatActivity {
+
+    Button submit;
+    EditText orgName;
+    EditText street;
+    EditText suburb;
+    EditText postcode;
+    EditText email;
+    EditText password;
+    EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_registration_page);
+
+        submit = findViewById(R.id.submitBtn);
+        orgName = findViewById(R.id.organisationNameET);
+        street = findViewById(R.id.streetET);
+        suburb = findViewById(R.id.suburbET);
+        postcode = findViewById(R.id.postcodeET);
+        email = findViewById(R.id.emailET);
+        password = findViewById(R.id.passwordET);
+        confirmPassword = findViewById(R.id.confirmPasswordET);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            String organisationName = String.valueOf( orgName.getText() );
+
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+
+            }
+        });
+
     }
+
+//    public boolean checkRequiredField() {
+//        String organisationName = String.valueOf( orgName.getText() );
+//
+//        if (!organisationName.equals("") &&
+//            !organisationName.equals("") &&
+//            !organisationName.equals("") &&
+//        ) return true;
+//        return false;
+//    }
+
 }
