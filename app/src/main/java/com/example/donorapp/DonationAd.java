99054ec790c21efe9b9c5ff7f07a63e88d4fd8ad
MatthewDBarrett <12948612@student.uts.edu.au>
2019-05-05 12:09:14 +1000

    DonationAd and BookingPage activities have been created

---
+++ app/src/main/java/com/example/donorapp/DonationAd.java
@@ -0,0 +1,13 @@
+package com.example.donorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationAd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_ad);
+    }
+}
